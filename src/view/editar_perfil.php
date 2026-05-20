7444a6859e52e7af9321f2be6f269bd5a66509a4
Johan <johanhenryag@gmail.com>
2026-05-20 10:35:02 +0200

    css editar perfil

--- src/view/editar_perfil.php
+++ src/view/editar_perfil.php
@@ -21,7 +21,7 @@ include_once 'header.php';
     <title>Editar Perfil | NightFest</title>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
-    <link rel="stylesheet" href="../assets/css/STYLE1.css">
+    <link rel="stylesheet" href="../assets/css/registro_estandar.css">
 </head>
 
 <body class="login-page">
